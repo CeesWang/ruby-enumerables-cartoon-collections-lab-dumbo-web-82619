4c97c55a932f518b692c3fc460b3ab9d964a152f
Chen Wang <wangcees@gmail.com>
2019-08-21 21:02:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,25 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)
+  index = 0
+  dwarves.each do |dwarve|
+  index += 1
+  puts "#{index} #{dwarve}"
+  end
+end
+
+def  summon_captain_planet(planets)
+  planets.map { |planet| planet.capitalize + "!" }
+
+# code an argument here
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.each do | call | 
+    if call.length > 4 
+      return true
+    end
+  end
+  false
 end
 
 def find_the_cheese# code an argument here
